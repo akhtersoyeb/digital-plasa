1994735ebb5d5d63aa69ba2813c2b061bfb15b5d
Sk Soyeb Akhter <sksoyebakhter2003@gmail.com>
2022-02-07 00:12:43 +0530

    added image drag-n-drop form in create-product page

--- pages/create-product.js
+++ pages/create-product.js
@@ -1,12 +1,46 @@
+import { Upload, message } from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
+
+import "antd/dist/antd.css"
+
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 export default function Example() {
+
+  const { Dragger } = Upload;
+
+  const props = {
+    name: 'file',
+    multiple: true,
+    listType: "picture",
+    maxCount: 3,
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    onChange(info) {
+      const { status } = info.file;
+      if (status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+    onDrop(e) {
+      console.log('Dropped files', e.dataTransfer.files);
+    },
+  };
+
   return (
-    <div className="container bg-slate-200 mx-auto rounded-xl my-20 py-5 px-5">
+    <>
+    <Navbar />
+    <div className="container bg-slate-200 mx-auto rounded-xl my-20 py-10 px-5">
       <div>
         <div className="md:grid md:grid-cols-3 md:gap-6">
           <div className="md:col-span-1">
             <div className="px-4 sm:px-0">
-              <h3 className="text-lg font-medium leading-6 text-gray-900">Product</h3>
+              <h3 className="text-lg font-medium leading-6 text-gray-900">New Product</h3>
               <p className="mt-1 text-sm text-gray-600">
                 This information will be displayed publicly so be careful what you share.
               </p>
@@ -22,7 +56,7 @@ export default function Example() {
                         Name
                       </label>
                       <div className="mt-1 flex rounded-md shadow-sm">
-                        
+
                         <input
                           type="text"
                           name="title"
@@ -53,39 +87,18 @@ export default function Example() {
                     </p>
                   </div>
 
-                
-
                   <div>
-                    <label className="block text-sm font-medium text-gray-700">Cover photo</label>
-                    <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                      <div className="space-y-1 text-center">
-                        <svg
-                          className="mx-auto h-12 w-12 text-gray-400"
-                          stroke="currentColor"
-                          fill="none"
-                          viewBox="0 0 48 48"
-                          aria-hidden="true"
-                        >
-                          <path
-                            d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                            strokeWidth={2}
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                        </svg>
-                        <div className="flex text-sm text-gray-600">
-                          <label
-                            htmlFor="file-upload"
-                            className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                          >
-                            <span>Upload a file</span>
-                            <input id="file-upload" name="file-upload" type="file" className="sr-only" />
-                          </label>
-                          <p className="pl-1">or drag and drop</p>
-                        </div>
-                        <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-                      </div>
-                    </div>
+                    <label className="block text-sm font-medium text-gray-700">Pictures</label>
+                    <Dragger {...props}>
+                      <p className="ant-upload-drag-icon">
+                        <InboxOutlined />
+                      </p>
+                      <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                      <p className="ant-upload-hint">
+                        Support for a single or bulk upload. Max limit 3.
+                      </p>
+                    </Dragger>,
+
                   </div>
                 </div>
                 <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
@@ -93,7 +106,7 @@ export default function Example() {
                     type="submit"
                     className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                   >
-                    Save
+                    Upload
                   </button>
                 </div>
               </div>
@@ -102,15 +115,9 @@ export default function Example() {
         </div>
       </div>
 
-      <div className="hidden sm:block" aria-hidden="true">
-        <div className="py-5">
-          <div className="border-t border-gray-200" />
-        </div>
-      </div>
-
-      
-
       
     </div>
+    <Footer />
+    </>
   )
 }
